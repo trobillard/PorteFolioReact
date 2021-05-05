35fcd0427b2aef859f8dffabf2fe4f3d55f1a576
Thibaud Robillard <thibaudrobillard@Thibauds-MacBook-Air.local>
2021-05-05 11:18:00 +0200

    navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './component/Header/Header';
 import Nav from './component/Nav/Nav';
 import Footer from './component/Footer/Footer';
 
 function App() {
   return (
-    <h2>Portfolio Project</h2>
+    <Nav/>
   );
 }
 
